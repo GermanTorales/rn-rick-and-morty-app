af8ada730e294a78488d1da3757c061d513a976b
German <german.torales@shopadvizor.com>
2021-02-08 19:28:52 -0300

    Add new functions

--- src/helpers/api/index.js
+++ src/helpers/api/index.js
@@ -1,11 +1,8 @@
 import { get } from "./get";
 
-export const getCharacters = async (page) => await get("character", page);
-export const getLocations = async () => await get("location");
-export const getEpisodes = async () => await get("episode");
+export const getCharacters = async (page) => await get({ option: "character", page });
+export const getLocations = async (page) => await get({ option: "location", page });
+export const getEpisodes = async () => await get({ option: "episode", page });
 
-export const getPage = async (fn, page) => {
-  const data = await fn(page);
-
-  return data;
-};
+export const getCharacter = async (id) => await get({ option: "character", id });
+export const getLocation = async (id) => await get({ option: "location", id });
