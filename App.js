96a43a3af2c51889283c29712d0c1a084f639aaa
German <german.torales@shopadvizor.com>
2021-02-08 19:26:03 -0300

    Use tab and stack navigation

--- App.js
+++ App.js
@@ -1,16 +1,38 @@
 import React from "react";
-import Home from "./src/Views/Home/Home";
+import Characters from "./src/Views/Characters/Characters";
 import SingleCharacter from "./src/Views/SingleCharacter/SingleCharacter";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { Dimensions } from "react-native";
-
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Locations from "./src/Views/Locations/Locations";
+import SingleLocation from "./src/Views/SingleLocation/SingleLocation";
 /* 
 Creo un stack de navegacion.
 Esta funcion devuelve 2 propiedades: la vista y el navegador
 */
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const Home = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Characters"
+        component={Characters}
+        options={() => ({
+          title: "All characters",
+          headerTitleStyle: {
+            fontWeight: "bold",
+            marginLeft: Dimensions.get("window").width / 4,
+          },
+        })}
+      />
+      <Tab.Screen name="Locations" component={Locations} />
+    </Tab.Navigator>
+  );
+};
 
 const App = () => {
   return (
@@ -25,20 +47,16 @@ const App = () => {
         Esta consta de un nombre y del componente que se va a mostrar
         Se le pueden agregar opciones de personalizacion a la barra
          */}
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={() => ({
-            title: "Rick and Morty",
-            headerTitleStyle: {
-              fontWeight: "bold",
-              marginLeft: Dimensions.get('window').width / 4
-            },
-          })}
-        />
+        <Stack.Screen name="Home" component={Home} />
+
         <Stack.Screen
           name="SingleCharacter"
           component={SingleCharacter}
+          options={({ route }) => ({ title: "Go back" })}
+        />
+        <Stack.Screen
+          name="SingleLocation"
+          component={SingleLocation}
           options={({ route }) => ({ title: "Go back" })}
         />
       </Stack.Navigator>
